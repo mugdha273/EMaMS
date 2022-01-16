5bcb0482e0e5da59ead07d924b92ad275f0ce633
mugdha273 <mugdhasharma0327@gmail.com>
2022-01-16 16:38:06 +0530

    localhost 3000 allowed

--- api/profiles_project/profiles_project/settings.py
+++ api/profiles_project/profiles_project/settings.py
@@ -125,3 +125,8 @@ AUTH_USER_MODEL = 'profiles_api.UserProfile'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'https://localhost:3000',
+]
